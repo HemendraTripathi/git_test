82fb72c688b38db586c87e4f61ce74b24fdb95f2
TheGreenArrow <57852366+HemendraTripathi@users.noreply.github.com>
2023-02-03 12:58:54 +0530

    Create count.kt

---
+++ count.kt
@@ -0,0 +1,20 @@
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var count = 0
+        val countText = findViewById(R.id.count_text) as TextView
+        val countButton = findViewById(R.id.count_button) as Button
+
+        countButton.setOnClickListener{
+            count += 1
+            countText.text = count.toString()
+        }
+    }
+}
